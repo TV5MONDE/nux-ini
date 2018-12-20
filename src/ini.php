117d3784e0dc9a02677b982cacbc42ac961983bc
Thomas Ballet Baz <thomas.ballet-baz@tv5monde.org>
2018-12-20 15:27:38 +0100

    adaptation to nginx

--- src/ini.php
+++ src/ini.php
@@ -1,5 +1,5 @@
 <?php
-if (FALSE !== ($nux_basedir = getenv('nux_basedir')) && file_exists($nux_basedir . 'ini.php'))
+if ((FALSE !== ($nux_basedir = getenv('nux_basedir')) || (!empty($_SERVER['nux_basedir']) && (FALSE !== ($nux_basedir = $_SERVER['nux_basedir'])))) && file_exists($nux_basedir . 'ini.php'))
 {
     require_once $nux_basedir . 'ini.php';
 }
