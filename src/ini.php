da79ecf3edd0f94dbda7ddad2d24e9ed0ce497c2
thomas2b <thomas2b@gmail.com>
2020-11-25 12:09:30 +0100

    adaptation to new version of nux

--- src/ini.php
+++ src/ini.php
@@ -1,5 +1,6 @@
 <?php
-if ((FALSE !== ($nux_basedir = getenv('nux_basedir')) || (!empty($_SERVER['nux_basedir']) && (FALSE !== ($nux_basedir = $_SERVER['nux_basedir'])))) && file_exists($nux_basedir . 'ini.php'))
+
+if ((false !== ($nux_basedir = getenv('nux_basedir')) || (!empty($_SERVER['nux_basedir']) && (false !== ($nux_basedir = $_SERVER['nux_basedir'])))) && file_exists($nux_basedir . 'init.php'))
 {
-    require_once $nux_basedir . 'ini.php';
+    require_once $nux_basedir . 'init.php';
 }
